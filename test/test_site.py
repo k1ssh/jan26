794b1dc9a9a9c9e9781a71a078ff2a73754818e8
k1ssh <kostyak382@gmail.com>
2026-01-15 17:06:00 +0300

    Tests

---
+++ test/test_site.py
@@ -0,0 +1,25 @@
+from itertools import product
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+from pages.homepage import HomePage
+from pages.product import ProductPage
+
+
+
+def test_open_s6(driver):
+    homepage = HomePage(driver)
+    homepage.open()
+    homepage.click_galaxy_s6()
+    product_page = ProductPage(driver)
+    product_page.check_title_is('Samsung galaxy s6')
+
+
+def test_two_monitors(driver):
+    homepage = HomePage(driver)
+    homepage.open()
+    homepage.click_monitor()
+    time.sleep(2)
+    homepage.check_products_count()
+
+
